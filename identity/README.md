8d67a9d1b102bc66855fbb2bba5623b1543988fb
Yuvraj <yuvraj.yad001@gmail.com>
2019-04-25 17:22:09 +0530

    identity package added

---
+++ identity/README.md
@@ -0,0 +1,9 @@
+Package identity provides type that allows to authorize request
+
+Download:
+```shell
+go get -u github.com/evalsocket/gophers-utility/identity
+```
+
+* * *
+Package identity provides type that allows to authorize request
---
+++ identity/identity.go
@@ -0,0 +1,43 @@
+/*
+Package identity provides type that allows to authorize request
+*/
+package identity
+
+import (
+	"github.com/google/uuid"
+)
+
+// Identity data to be encode in auth token
+type Identity struct {
+	ID    uuid.UUID `json:"id"`
+	Email string    `json:"email"`
+	Roles []string  `json:"roles"`
+}
+
+// WithEmail returns a new Identity with given email value
+func WithEmail(email string) (*Identity, error) {
+	i, err := New()
+	if err != nil {
+		return nil, err
+	}
+	i.Email = email
+
+	return i, nil
+}
+
+// WithValues returns a new Identity for given values
+func WithValues(id uuid.UUID, email string, roles []string) *Identity {
+	return &Identity{id, email, roles}
+}
+
+// New returns a new Identity
+func New() (*Identity, error) {
+	id, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Identity{
+		ID: id,
+	}, nil
+}
